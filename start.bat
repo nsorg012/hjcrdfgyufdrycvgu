02afb67db78427a3a7a422b85e58293705801db2
nsorg012 <88821750+nsorg012@users.noreply.github.com>
2021-08-12 14:38:55 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -16,6 +16,6 @@ echo Success!
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
 echo Username: administrator
-echo Password: Mirsmirs@1
+echo Password: JohnTech1234
 echo You can login now.
 ping -n 10 127.0.0.1 >nul
